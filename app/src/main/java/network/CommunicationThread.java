ceb8bbdf358938ef2c998632817fe712149d4b66
PsstBehindU <tarqinius@gmail.com>
2020-05-21 14:28:52 +0300

    completare commsrv

--- app/src/main/java/network/CommunicationThread.java
+++ app/src/main/java/network/CommunicationThread.java
@@ -1,6 +1,16 @@
 package network;
 
+import android.util.Log;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import general.Constants;
+import general.Utilities;
 
 public class CommunicationThread extends Thread{
     private ServerThread serverThread;
@@ -9,5 +19,37 @@ public class CommunicationThread extends Thread{
     public CommunicationThread(ServerThread serverThread, Socket socket) {
         this.serverThread = serverThread;
         this.socket = socket;
+    }
+
+    public void run() {
+        if (socket == null) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+            return;
+        }
+
+        try {
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
